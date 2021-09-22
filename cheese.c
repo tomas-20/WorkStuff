82f04d8cffec7e497d02ea6c3c748c8375a41d64
tomas-20 <tacuna20@stuy.edu>
2021-09-21 23:00:04 -0400

    did some fun math operations

--- cheese.c
+++ cheese.c
@@ -2,3 +2,10 @@
 void print_number(int n) {
   printf("%d\n", n);
 }
+int main() {
+  int bob = 2187;
+  int joe = 276;
+  print_number(bob + joe);
+  print_number(bob * joe);
+  print_number(bob / joe);
+}
