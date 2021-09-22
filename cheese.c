eed74d969a0c3352f7ef5a747d87dd826a8c6296
tomas-20 <tacuna20@stuy.edu>
2021-09-21 23:04:30 -0400

    added a fun exponent function cause apparently ** is not a thing

--- cheese.c
+++ cheese.c
@@ -2,10 +2,19 @@
 void print_number(int n) {
   printf("%d\n", n);
 }
+int expt(int a, int b) {
+  if (b == 0) {
+    return 1;
+  }
+  else {
+    return a * expt(a, b - 1);
+  }
+}
 int main() {
   int bob = 2187;
   int joe = 276;
   print_number(bob + joe);
   print_number(bob * joe);
   print_number(bob / joe);
+  print_number(expt(3, 7));
 }
